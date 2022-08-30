3b47f3d3f282e18f5d3dbadc23c50bfc50c64511
Minskoleopc <minskoleopc@gmail.com>
2022-08-30 08:27:34 +0530

    day1

---
+++ script.js
@@ -0,0 +1,23 @@
+//Step1 :- 
+// Download and Install Node js
+// https://nodejs.org/en/download/
+// Step 2:- 
+// Download and Install Visual Studio Code
+// https://code.visualstudio.com/download
+// Step3 : - Create folder on Desktop - Javascript15
+// Step 4 : Open the folder with visual studio Code
+// Step 5 : Create a script.js in same folder
+// Step 6 : Open Terminal and Run file  node script.js
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ string.js
@@ -0,0 +1,53 @@
+let firstName = "chinmay"
+console.log(firstName)
+console.log(typeof firstName)
+
+
+let x = "hello"
+let x1 = 'hello1'
+let x2 = `hello2`
+
+console.log(x)
+console.log(x1)
+console.log(x2)
+
+
+// number + number =====> number 
+// string + number =====> string 
+// number + string =====> string 
+// string + string =====> string
+
+let a = 10
+let b = 20
+let c = "hello"
+let d = "h1"
+
+console.log(a+b)
+console.log(c+b)
+console.log(a+c)
+console.log(c+b)
+console.log(a+b+c)
+
+// number + number + string 
+//  10     + 20    + hello
+//       30        + hello 
+//'30hello'
+console.log(c+b+a)
+
+// string + number + number
+// 'hello'+  20    + 10
+//    'hello20'    + 10
+//      'hello2010'
+ 
+
+// string concat
+
+let firstN = "chinmay"
+let lastN = "deshpande"
+// My firstName is chinmay and last Name is deshpande
+
+// old way 
+console.log("My firstName is "+firstN+" and lastName is "+lastN)
+// new way
+console.log(`My firstName is ${firstN} and lastName is ${lastN}`)
+
