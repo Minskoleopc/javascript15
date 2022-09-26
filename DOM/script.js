990364ab81cc67a200ede7412b29fcd949c1c5c3
Minskoleopc <minskoleopc@gmail.com>
2022-09-26 08:32:40 +0530

    message

---
+++ DOM/script.js
@@ -0,0 +1,18 @@
+
+// index.html  ==> html hypertext markup language
+// style.css   ==> cascading style sheet
+// script.js   ==> javascript
+
+// attach style file to html page
+// attach script file to html page
+
+
+document.querySelector('h1').addEventListener('click',function(){
+    document.querySelector('h1').style.color = "yellow"
+})
+
+// client --- html    css   javascript 
+
+//html --- text
+//css  ---- design 
+//js   ----- change soemthing on user action javascript
