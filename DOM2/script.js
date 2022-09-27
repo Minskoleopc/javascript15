1027f908148835c8b843aa9a94e17274c9e5b66d
Minskoleopc <minskoleopc@gmail.com>
2022-09-27 20:05:28 +0530

    added

--- DOM2/script.js
+++ DOM2/script.js
@@ -1,25 +1,18 @@
-
 document.querySelector('h1')
 .addEventListener('click',function(){
     document.querySelector('h1').textContent = "ram"
 })
-
 //<p id ="paraOne" class = "paraTwo" name = "paraThree" >Retrive element</p>
-
 // tagName 
-
 let byTagName = document.querySelector('p')
 console.log(byTagName)
-
 // id
 let byId = document.querySelector('#paraOne')
 console.log(byId)
-
 // class
 let byClass = document.querySelector('.paraTwo')
 console.log(byClass)
 // commonformula
 // tagName[attribute="value"]
-
 let byCommonF = document.querySelector('p[name="paraThree"]')
 console.log(byCommonF)
